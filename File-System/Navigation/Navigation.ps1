d48c5a5608e9e2a7e8a7fca5d4ee26f09726c8f5
Junior <g2music16@gmail.com>
2026-08-14 09:29:03 -0500

    Add descriptions and more navigation commands

--- File-System/Navigation/Navigation.ps1
+++ File-System/Navigation/Navigation.ps1
@@ -1,9 +1,14 @@
+# Show curret directory
 Get-Location
 
+# List contents of current directory
 Get-ChildItem
 
+# Change directory
 Set-Location C:\
 
+# Test whether a that exist
 Test-Path C:\Lab
 
+# Show hidden items also in directory
 Get-ChildItem -Force

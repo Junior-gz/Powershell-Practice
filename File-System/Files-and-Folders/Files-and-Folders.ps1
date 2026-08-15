e01ce3090c16694defb871ddd24c54fdccf3fe08
Junior <g2music16@gmail.com>
2026-08-15 12:06:28 -0500

    Add file and folder reference commands

---
+++ File-System/Files-and-Folders/Files-and-Folders.ps1
@@ -0,0 +1,18 @@
+# Creates a new file or folder
+New-Item
+
+# Copies a file or folder
+Copy-Item
+
+# Moves a file or folder
+Move-Item
+
+# Deletes a file or folder
+Remove-Item
+
+# Displays the content of a text file
+Get-Content
+
+# Renames a file or folder
+Rename-Item
+

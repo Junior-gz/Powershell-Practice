b7545b959f5fc269b6021307206c2b12f5de098b
Junior <g2music16@gmail.com>
2026-08-17 13:22:07 -0500

    Add references to Enviroment-Variables for permanent variables

--- Enviroment/Enviromental-Variables/Enviroment-Variables.ps1
+++ Enviroment/Enviromental-Variables/Enviroment-Variables.ps1
@@ -1,2 +1,11 @@
+*Anything in between the stars are examples
+
+
 # List Environment variables
 Get-ChildItem Env:
+
+# Creates a session variable
+$env:*PSP* = "*C:\Users\G2mus\Powershell-Practice*"
+
+# Creates a permanent variable
+[Enviroment]::SetEnviromentVariable("*PSP*", "*C:\Users\G2mus\Powershell-Practice*", "User")

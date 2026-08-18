9adc712613c0dc69ba45a1015a9ece8ccf980341
Junior <g2music16@gmail.com>
2026-08-18 14:56:46 -0500

    Add shut down references to System-Info

--- System/System-Info/System-Info.ps1
+++ System/System-Info/System-Info.ps1
@@ -6,3 +6,9 @@ Clears the terminal, keeps the directory
 
 # Prints/repeats the text you write
 Write-Output "text" 
+
+# Restarts computer
+Restart-Computer
+
+#Restarts the computer to recovery mode instantly
+Shutdown /R /O /T 0

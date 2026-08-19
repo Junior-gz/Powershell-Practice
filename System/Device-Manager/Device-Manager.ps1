a781f53f214e4de4c9c958dba6b954694deb4f47
Junior <g2music16@gmail.com>
2026-08-19 14:08:40 -0500

    Add more references to Device_Manager.ps1, Add single command reference to System-Info.ps1

--- System/Device-Manager/Device-Manager.ps1
+++ System/Device-Manager/Device-Manager.ps1
@@ -7,8 +7,11 @@ Get-PnpDevice
 # Filters to devices showing a problem
 Get-PnpDevice -Status Error
 
+# Shows unknown devices
+Get-PnpDevice -Status Error
+
 # Filters to a specific device catagory
-Fet-PnpDevice -Class "*Net*"
+Get-PnpDevice -Class "*Net*"
 
 # All the class names in the system
 Get-PnpDevice | Select-Object -Property Class -Unique
@@ -18,3 +21,21 @@ Get-PnpDeviceProperty -InstanceId <DeviceID> -KeyName "*DEVPKEY_Device_DriverVer
 
 # Shows how many devices there are
 Get-PnpDevice | Measure-Object
+
+# Shows device hardware ID
+Get-PnpDevice | Where-Object {$_.FriendlyName -like "*NVIDIA*"} | Select-Object FriendlyName, Class, Status, InstanceId (Needs star, but is still a example)
+
+# Shows the friendly name of the device
+Get-PnpDevice -FriendlyName "*Integrated Camera*" (Needs star, but is still a example)
+
+# Expands ID
+Get-PnpDevice -FriendlyName "*Integrated camera*" | Select-Object -ExpandProperty InstanceId
+
+# Disables Device
+Disable-PnpDevice -InstanceId "<InstanceID>" -Confirm:$false
+
+# Enables Device
+Enable-PnpDevice -InstanceId "<InstanceID>" -Confirm:$false
+
+# Dumps every property of device
+Get-PnpDevice -FriendlyName "*Integrated camera*" | Format-List * (Stars are needed but still an example)
--- System/System-Info/System-Info.ps1
+++ System/System-Info/System-Info.ps1
@@ -12,3 +12,6 @@ Restart-Computer
 
 #Restarts the computer to recovery mode instantly
 Shutdown /R /O /T 0
+
+# Stars a Administrater Powersell session
+Start-Process powershell -Verb RunAs

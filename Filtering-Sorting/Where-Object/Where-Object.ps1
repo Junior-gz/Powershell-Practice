eee702e427eb7261b37c384a6b52c1199b77e2c3
Junior <g2music16@gmail.com>
2026-09-01 15:22:47 -0500

    Add more references to Where-Object

--- Filtering-Sorting/Where-Object/Where-Object.ps1
+++ Filtering-Sorting/Where-Object/Where-Object.ps1
@@ -3,3 +3,9 @@ Get-Process | Where-Object { $_.WorkingSet -gt 100MB }
 
 # Filter services that are currently running
 Get-Service | Where-Object { $_.Status -eq "Runnning" }
+
+# Simplified syntax (Powershell 3.0+) - no scrit block needed for simple comparisons
+Get-Process | Where-Object WorkingSet -gt 100MB
+
+# Combine multiple conditions
+Get-Process | Where-Object { $_.WorkingSet -gt 100MB -and $_.CPU -gt 10 }
